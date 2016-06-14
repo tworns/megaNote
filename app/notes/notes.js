ea0c99906875ede074548536550879ca19797f9c
tworns <tworns@yahoo.com>
2016-06-14 09:36:27 -0400

    Prettied up note displays and noted a few things about how Angular works

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -9,22 +9,25 @@ angular.module('meganote.notes', [
   .state('notes', {
       url: '/notes',
       templateUrl: '/notes/notes.html',
-      controller: 'NotesController'
+      controller: 'NotesController' //controller is inherited by child state
     })
 
-  .state('notes.form', {
+  .state('notes.form', { //this is a child state
     url: '/:noteId',
     templateUrl: 'notes/notes-form.html'
   });
 })
 
-.controller('NotesController', function($scope) {
+.controller('NotesController', function($scope) {//defines functionality for a given scope
   $scope.editing = false;
   $scope.notes = [];
-
+  $scope.note = {
+    title: "",
+    data: ""
+  };
   $scope.addNote = function(){
     $scope.notes.push($scope.note);
-      $scope.note = { title: '', data: '' };
+    $scope.note = { title: '', data: '' };
   };
 
 });
