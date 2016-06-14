515f476ba6ed99caf6e53e8edae690546f6d2c14
tworns <tworns@yahoo.com>
2016-06-14 11:20:32 -0400

    Made eslint's indentaion happy

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,46 +1,46 @@
 (function(){
-    angular
+  angular
     .module('meganote.notes', ['ui.router'])
     .config(notesConfig)
     .controller('NotesController', noteCtrl); //controller name won't conflict with variable names. Controllers define funcitonality for given scope.
 
-    notesConfig.$inject = ['$stateProvider']; //contains arguments to notes config IN ORDER!
+  notesConfig.$inject = ['$stateProvider']; //contains arguments to notes config IN ORDER!
 
-    function notesConfig($stateProvider) {
-      $stateProvider
+  function notesConfig($stateProvider) {
+    $stateProvider
 
       .state('notes', {
-          url: '/notes',
-          templateUrl: '/notes/notes.html',
-          controller: 'NotesController' //controller is inherited by child state
-        })
+        url: '/notes',
+        templateUrl: '/notes/notes.html',
+        controller: 'NotesController' //controller is inherited by child state
+      })
 
       .state('notes.form', { //this is a child state
         url: '/:noteId',
         templateUrl: 'notes/notes-form.html'
       });
-    }
-      noteCtrl.$inject = ['$scope', '$state', 'NotesService'];
-    function noteCtrl ($scope, $state, NotesService) {
-      $state.go('notes.form');
-      NotesService.getNotes().then(function(){
-        $scope.notes = NotesService.notes;
-      });
+  }
+  noteCtrl.$inject = ['$scope', '$state', 'NotesService'];
+  function noteCtrl ($scope, $state, NotesService) {
+    $state.go('notes.form');
+    NotesService.getNotes().then(function(){
+      $scope.notes = NotesService.notes;
+    });
 
-      $scope.editing = false;
-      $scope.notes = [];
-      $scope.note = {
-        title: "",
-        data: ""
-      };
-      $scope.addNote = function(){
-        $scope.notes.push($scope.note);
-        $scope.note = { title: '', data: '' };
-      };
-      $scope.edit = function(note){
-        $scope.note = note;
-      };
-    }
+    $scope.editing = false;
+    $scope.notes = [];
+    $scope.note = {
+      title: '',
+      data: ''
+    };
+    $scope.addNote = function(){
+      $scope.notes.push($scope.note);
+      $scope.note = { title: '', data: '' };
+    };
+    $scope.edit = function(note){
+      $scope.note = note;
+    };
+  }
 
 
 })();
